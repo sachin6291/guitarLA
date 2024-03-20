c3870111228b29e5ee0c1500f4f17f0fc811a8c2
Sachin Tekchandani <sachin6291@gmail.com>
2024-03-20 22:59:07 +0000

    some progress

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,9 @@ const addToCart=(item)=>{
   return (
     <>
       
-    <Header/>
+    <Header
+      cart={cart}
+    />
 
     <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-const Header = () => {
+const Header = ({cart}) => {
   return (
     <header className="py-5 header">
         <div className="container-xl">
@@ -27,38 +27,40 @@ const Header = () => {
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <tr>
-                                        <td>
-                                            <img className="img-fluid" src="/img/guitarra_02.jpg" alt="imagen guitarra" />
-                                        </td>
-                                        <td>SRV</td>
-                                        <td className="fw-bold">
-                                                $299
-                                        </td>
-                                        <td className="flex align-items-start gap-4">
-                                            <button
-                                                type="button"
-                                                className="btn btn-dark"
-                                            >
-                                                -
-                                            </button>
-                                                1
-                                            <button
-                                                type="button"
-                                                className="btn btn-dark"
-                                            >
-                                                +
-                                            </button>
-                                        </td>
-                                        <td>
-                                            <button
-                                                className="btn btn-danger"
-                                                type="button"
-                                            >
-                                                X
-                                            </button>
-                                        </td>
-                                    </tr>
+                                    {cart.map(item=>(
+                                        <tr>
+                                            <td>
+                                                <img className="img-fluid" src={`/img/${item.image}.jpg`} alt="imagen guitarra" />
+                                            </td>
+                                            <td>{item.name}</td>
+                                            <td className="fw-bold">
+                                                    ${item.price}
+                                            </td>
+                                            <td className="flex align-items-start gap-4">
+                                                <button
+                                                    type="button"
+                                                    className="btn btn-dark"
+                                                >
+                                                    -
+                                                </button>
+                                                    {item.quantity}
+                                                <button
+                                                    type="button"
+                                                    className="btn btn-dark"
+                                                >
+                                                    +
+                                                </button>
+                                            </td>
+                                            <td>
+                                                <button
+                                                    className="btn btn-danger"
+                                                    type="button"
+                                                >
+                                                    X
+                                                </button>
+                                            </td>
+                                        </tr>
+                                    ))}
                                 </tbody>
                             </table>
 
